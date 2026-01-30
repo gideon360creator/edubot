0d2c48e7d0137e733c0479bc7f31c69abce3b35f
gideon <younghenry360@gmail.com>
2026-01-30 20:30:41 +0100

    added vercel analytics to client

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -6,6 +6,7 @@ import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 import type { QueryClient } from '@tanstack/react-query'
 import type { AuthState } from '../auth/types'
 import { Toaster } from '@/components/ui/sonner'
+import { Analytics } from '@vercel/analytics/react'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -17,6 +18,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <main className="select-none">
       {/* <Header /> */}
       <Outlet />
+      <Analytics />
       <Toaster closeButton />
       <TanStackDevtools
         config={{
